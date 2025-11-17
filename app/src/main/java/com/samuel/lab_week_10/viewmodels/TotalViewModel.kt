b1e0702fbb82a541cb6b828868131c57c8712a7b
MBotakk <samuel.itachis4@gmail.com>
2025-11-17 23:02:52 +0700

    Commit No. 2: Implement LiveData to Sync Activity and Fragment UI

--- app/src/main/java/com/samuel/lab_week_10/viewmodels/TotalViewModel.kt
+++ app/src/main/java/com/samuel/lab_week_10/viewmodels/TotalViewModel.kt
@@ -1,16 +1,25 @@
 package com.samuel.lab_week_10.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TotalViewModel : ViewModel() {
-    val total = MutableLiveData<Int>()
+    //Declare the LiveData object
+    private val _total = MutableLiveData<Int>()
+    val total: LiveData<Int> = _total
 
     init {
-        total.value = 0
+        //Initialize the LiveData object
+        //postValue is used to set the value of the LiveData object
+        //from a background thread or the main thread
+        //while on the other hand setValue() is used
+        //only if you're on the main thread
+        _total.postValue(0)
     }
 
+    //Increment the total value
     fun incrementTotal() {
-        total.value = (total.value ?: 0) + 1
+        _total.postValue((_total.value?:0) + 1)
     }
 }
